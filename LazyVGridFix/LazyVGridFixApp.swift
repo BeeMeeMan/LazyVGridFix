2240b6514f7b47807eaf58ff3a33f99b771f210b
Korsun <jenya.korsun@gmail.com>
2023-12-06 10:59:09 +0100

    Initial Commit

---
+++ LazyVGridFix/LazyVGridFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridFixApp.swift
+//  LazyVGridFix
+//
+//  Created by Yevhenii Korsun on 06.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
